2077b0c50c912e1507cac16901914b9eb00a4f9e
Christian Viana <christian.viana@acct.global>
2022-11-07 08:46:54 -0300

    Iniciado projeto

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import Logo from '../assets/logo.png'
+import { Moon } from 'phosphor-react'
+export function Header(){
+    return (
+        <div className='sm:grid grid-cols-3 mt-2'>
+            <img src={Logo} alt="Logo" className='w-44'/>
+            <div>
+                <ul className='flex gap-4 justify-center text-font-300'>
+                    <li>Services</li>
+                    <li>Portfolios</li>
+                    <li>Experience</li>
+                </ul>
+            </div>
+            <div className='flex gap-2 items-center justify-end'>
+                <Moon size={24} color="#FFF"/>
+                <button className='text-font-300'>Resume</button>
+            </div>
+        </div>
+    )
+
+}
