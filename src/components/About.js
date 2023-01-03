8a89cb87eecb07ddb765fedc7f7a42ebac5fb7fa
Christian L. Viana <viana2015christian@gmail.com>
2023-01-02 22:14:31 -0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -10,10 +10,7 @@ export function About() {
         <h1 className="font-plus text-font-400 text-[3rem]">Christian <span className="text-font-verde">Viana</span></h1>
         <p className="text-[#232E35]">
 
-          Developer since 2017 where I developed my first applications
-          using COS (Caché Object Script) and CSP (Caché Server Pages) acting directly as
-          Full Stack.
-          Today I work directly as Front End, but I always apply my studies in back-end and front-end technologies
+          Developer since 2017 where I hade my first experience utilized COS (Caché Object Script) and CSP (Caché Server Pages) acting directly as Full Stack. Today I work directly as Front End, but I always apply my studies in back-end and front-end technologies
         </p>
         <div className="flex mt-8 gap-2">
           <a href="https://www.instagram.com/chrisviana_dev/" className="hover:text-font-verde">
